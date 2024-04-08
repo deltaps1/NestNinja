0683012d0ff9f5faafc591985afa93c404247d12
Karsten Skjellerup <deltapsi@icloud.com>
2024-04-08 23:33:33 +0200

    Small cleanup in tests

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,11 +1,18 @@
 from NestNinja import Navigator
 from test_utils import get_test_data 
 
+
+def get_nav_base():
+    data = get_test_data()
+    nav_base = Navigator(data)
+    return nav_base
+
+
 def test_getitem_method_int():
     """Gets one dictionary from the list of dictionaries and asserts it is a dictionary."""
     nav_base = get_nav_base()
     assert isinstance(nav_base[0], dict)
-    ...
+
 
 def test_navigation():
     """The test case navigatese through the JSON-object. 
@@ -22,8 +29,6 @@ def test_navigation():
     assert len(nav["cvrNummer"]) > 0
 
 
-def get_nav_base():
-    data = get_test_data()
-    nav_base = Navigator(data)
-    return nav_base
+def test_promote():
+    base = get_nav_base()
 
