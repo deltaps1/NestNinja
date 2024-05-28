f0d8d62dcaf2ca3b944417f34a28f388a6277960
Karsten <deltapsi@icloud.com>
2024-05-29 01:28:52 +0200

    Double newlines

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -10,6 +10,7 @@ def get_copy_of_base_object():
     """Creates a copy of the base object"""
     return BASE_OBJECT.copy()
 
+
 def navigate_to_actual_data():
     """Navigates through the data structure to the interesting part of the data"""
     nav_base = get_copy_of_base_object()
@@ -22,17 +23,20 @@ def navigate_to_actual_data():
     )
     return nav
 
+
 def test_copy1():
     """Test that the copy functionality returns a new instance"""
     base = get_copy_of_base_object()
     new = base.copy()
     assert id(base.data) != id(new.data)
     
+
 def test_copy2():
     """Test that the copy functionality returns a new instance"""
     base = get_copy_of_base_object()
     new = base.copy()
     assert id(base.data[0]) != id(new.data[0])
+
 
 def test_getitem_method_int():
     """Gets one dictionary from the list of dictionaries and asserts it is a dictionary."""
@@ -47,6 +51,7 @@ def test_navigation():
     nav = navigate_to_actual_data()
     assert len(nav["cvrNummer"]) > 0
 
+
 def test_promote():
     """Checks that promotion functions. Promotes a key from the test data
     and asserts that a given key is in newly created `Navigator`.
@@ -55,6 +60,7 @@ def test_promote():
     promoted = base.promote('virksomhedMetadata', "meta_")
     promoted_anaysed = promoted.analyse()
     assert "meta_nyesteNavn" in promoted_anaysed.data
+
 
 if __name__ == "__main__": 
     testobj = test_navigation()
