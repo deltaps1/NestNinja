e50b801295cc401f0dc9178dc5fc59957c71e291
Karsten <deltapsi@icloud.com>
2024-05-29 01:28:00 +0200

    Added double newlines between functions

--- test_utils.py
+++ test_utils.py
@@ -3,12 +3,15 @@ from pathlib import Path
 
 PATH = Path("tests/test_data/")
 
+
 def open_json_file(path):
     with open(path) as f: return json.load(f)
+
 
 def get_test_data():
    files = PATH.glob("*.json") 
    return [open_json_file(file) for file in files]
+
 
 if __name__ == '__main__':
     all_files = get_test_data()
