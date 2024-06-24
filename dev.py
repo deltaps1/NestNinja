2a0b4a531616abf89d11795bb89c36009720858a
Karsten <deltapsi@icloud.com>
2024-06-24 16:58:37 +0200

    Init dev for testing purposes

---
+++ dev.py
@@ -0,0 +1,8 @@
+"""
+This document is for testing the funtionality of NestNinja on a large dataset
+"""
+from test_utils import get_test_data
+from NestNinja import Navigator
+
+test_data = get_test_data()
+nav = Navigator(test_data)
